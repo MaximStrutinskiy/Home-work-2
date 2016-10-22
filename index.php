bee9640acce5353b9a29531fa4229ae09111e04a
MaximStrutinskiy <max.strutinskiy>
2016-10-22 13:26:17 +0300

    install and use lib - Carbon

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
-<?
+<?php
 echo "<h1>Home work 2: install composer, php libraries. Work with Git</h1>";
+
+//Carbon - show date
+echo "<h2>Carbon - show date</h2>";
+
+require 'vendor/autoload.php';
+
+use Carbon\Carbon;
+
+printf("Now: %s", Carbon::now());
+
+echo "<br/><br/>";
 
 
