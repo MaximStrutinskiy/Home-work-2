e296afc4473eafd8d266cc814f092797a3d19027
MaximStrutinskiy <max.strutinskiy>
2016-10-30 00:24:53 +0300

    fix home-work-2

--- index.php
+++ index.php
@@ -1,23 +1,16 @@
 <?php
-echo "<h1>Home work 2: install composer, php libraries. Work with Git</h1>";
-
-//Carbon - show date
-echo "<h2>Carbon - show date</h2>";
 
 require 'vendor/autoload.php';
+
+//Carbon - show date
 
 use Carbon\Carbon;
 
 printf("Now: %s", Carbon::now());
 
-echo "<br/><br/>";
-
-
 //Faker - show random data
-echo "<h2>Faker - show random data</h2>";
+
 $faker = Faker\Factory::create();
-
-
 
 for ($i=0; $i < 10; $i++) {
   echo $faker->name;
