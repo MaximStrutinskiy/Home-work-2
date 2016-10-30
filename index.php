342cd3ee5e9e9c672f3c538b854f053cd33ac0ec
Maxim Strutinskiy <max.strutinskiy@gmail.com>
2016-10-30 15:01:47 +0200

    Update index.php

--- index.php
+++ index.php
@@ -14,13 +14,7 @@ $faker = Faker\Factory::create();
 
 for ($i=0; $i < 10; $i++) {
   echo $faker->name;
-  echo "<br/>";
   echo $faker->address;
-  echo "<br/>";
   echo $faker->text;
-  echo "<br/>";
-  echo "<br/>";
-  echo "<br/>";
-  echo "<br/>";
 }
 
